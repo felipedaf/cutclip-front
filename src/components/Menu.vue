1296d6e355171050674075c7110c8790a4845755
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2020-07-08 20:22:48 -0300

    Menu addded!

---
+++ src/components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <transition name="enter">
+        <div class="menu__body"></div>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+<style lang="scss">
+
+
+.menu__body {
+    width: 30vw;
+    height: 100%;
+    background-color: black;
+}
+
+</style>
