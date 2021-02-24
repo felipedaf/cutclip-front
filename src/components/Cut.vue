12bbfec2a1f44bb1abd69637cd78bb3d4742724d
FelipeAmorimFerreira <felipe.amorim.ferreira@ccc.ufcg.edu.br>
2021-02-24 19:00:44 -0300

    Cut interactive icons area created

---
+++ src/components/Cut.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="cut__container">
+
+
+    </div>
+</template>
+<script>
+export default {
+    name: "Cut",
+    props: {
+        meta: Object
+    }
+
+}
+</script>
+<style lang="scss" scoped>
+
+.cut__container {
+    min-width: 100px;
+    height: 100px;
+    margin: 20px auto;
+
+}
+
+</style>
--- src/components/Cuts.vue
+++ src/components/Cuts.vue
@@ -3,15 +3,36 @@
         <div class="cuts__title-area">
             <span class="cuts__title">CUTS</span>
         </div>
+        <div class="cuts__fixes-area">
+            <div class="cuts__icon-area">
+                <template v-for="meta in this.cutsMeta">
+                    <Cut :meta="meta" :key="meta.hash"/>
+                </template>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+import Cut from "./Cut.vue"
 export default {
-    name: "Cuts"
+    name: "Cuts",
+    components: {
+        Cut
+    },
+    data: function() {
+        return {
+            cutsMeta: []
+        }
+    },
+    created: function() {
+        const cuts = localStorage.cuts
+        this.cutsMeta = JSON.parse(cuts)
+        console.log(this.cutsMeta)
+    }
 }
 
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .cuts__container {
     height: 36vw;
     width: 22vw;
@@ -20,6 +41,8 @@ export default {
     max-height: 540px;
     max-width: 500px;
     border-radius: 10px;
+    display: flex;
+    flex-direction: column;
 
 }
 
@@ -35,5 +58,19 @@ export default {
     font-weight: 900;
     color: #292929;
     text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
+}
+
+.cuts__icon-area {
+    box-sizing: border-box;
+    height: max-content;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    padding-bottom: 20px;
+}
+
+.cuts__fixes-area {
+    overflow: auto;
+    height: 100%;
 }
 </style>
